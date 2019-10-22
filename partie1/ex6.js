04a7e6eec12290fc4797c218ae281429c42702bd
Robin.Habran <robinhabran@gmail.com>
2019-10-22 16:30:29 +0200

    ex1 ex2 ex3 ex4

---
+++ partie1/ex6.js
@@ -0,0 +1,5 @@
+// fonction avec retour entier du reste de la dividion euclidienne
+function showModulo(firstNumber,secondNumber){
+  //'%' sigle de modulo = reste de la division euclidienne
+  return firstNumber%secondNumber;
+}
