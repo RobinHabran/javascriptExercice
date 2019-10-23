c267e5106e5641844c90007a512004d429447d68
Robin.Habran <robinhabran@gmail.com>
2019-10-23 16:21:05 +0200

    correction ex5 et 6

--- partie1/ex6.js
+++ partie1/ex6.js
@@ -3,3 +3,23 @@ function showModulo(firstNumber,secondNumber){
   //'%' sigle de modulo = reste de la division euclidienne
   return firstNumber%secondNumber;
 }
+
+// appel de la fonction 'showModulo' créée au préalable dans le fichier js
+document.getElementById('submit').addEventListener('click',alertShowModulo);
+function alertShowModulo(){
+  var firstNumber = document.getElementById('firstNumber').value;
+  var secondNumber = document.getElementById('secondNumber').value;
+  // cas où ce n'est pas un nombre
+  if ((isNaN(firstNumber) == true) && (isNaN(secondNumber) == true)){
+    alert('Veuillez n\'introduire que des nombres.');
+  //cas où au moins l'un des champs n'a pas été rempli
+  }else if (!firstNumber && !secondNumber) {
+    alert('Au moins un des champs n\'a pas été rempli.');
+  // cas où l'on essaye de diviser par 0
+  }else if (secondNumber==0) {
+    alert('vous ne pouvez pas entrer 0 dans le diviseur')
+  // cas valide = nombre, par 0 en diviseur
+  }else {
+    alert('le reste de la division entière de '+firstNumber+' par '+secondNumber+' est de : '+ showModulo(firstNumber,secondNumber));
+  }
+}
---
+++ partie1/ex7.js
@@ -0,0 +1,25 @@
+function schmilblic(shoeSize, yearOfBirth){
+  parseInt(shoeSize);
+  parseInt(yearOfBirth);
+  var result;
+/*  var result = shoeSize *2;
+  result += 5;
+  result *= 50;
+  result -= yearOfBirth;
+  result += 1766;
+*/
+  return result = (shoeSize*2+5)*50-yearOfBirth+1766;
+}
+
+document.getElementById('submit').addEventListener('click' , appelSchmilblic);
+function appelSchmilblic(){
+  var shoeSize   = document.getElementById('shoeSize').value;
+  var yearOfBirth = document.getElementById('yearOfBirth').value;
+  var result;
+
+  // cas de la pointure
+  if (isNaN(shoeSize)==false && isNaN(yearOfBirth)==false) {
+    parseInt(result = schmilblic(shoeSize , yearOfBirth));
+    alert(result);
+  }
+}
