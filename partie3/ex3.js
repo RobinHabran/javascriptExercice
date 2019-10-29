8098a6d15fe40ec8b2cd3b3d80139262c58faccf
Robin.Habran <robinhabran@gmail.com>
2019-10-29 10:15:10 +0100

    correction partie3 & avancement partie 4

--- partie3/ex3.js
+++ partie3/ex3.js
@@ -3,11 +3,10 @@ function changeColor(colorSquare){
   // appels test dans la console:
   console.log(colorSquare.className);
   console.log(document.getElementById('text'));
+
+  // 'colorSquare.className.replace('color ','')' = remplace le string 'color ' par rien. Ce qui renvoie à faire une suppression
+  document.getElementById('text').style.color = colorSquare.className.replace('color ','');
 /*
-  var myColor = colorSquare.className - 'color ';
-  console.log(typeof myColor);
-  document.getElementById('text').style.color = myColor;
-*/
   // 'colorSquare.className' = récupère le nom des class de l'objet
   if (colorSquare.className == 'color green') {
     // assigne la couleur green à l'attribut html style de l'attribut css color
@@ -16,6 +15,5 @@ function changeColor(colorSquare){
     document.getElementById('text').style.color = 'red';
   }else if(colorSquare.className == 'color blue'){
     document.getElementById('text').style.color = 'blue';
-  }
-
+  } */
 }
--- partie4/Exercices-JavaScript-Partie-4-master/questions.js
+++ partie4/Exercices-JavaScript-Partie-4-master/questions.js
@@ -4,36 +4,48 @@
  * Il peut y avoir plusieur façon de faire.
  */
 var tailleString = function (texte) {
+  // '.length' renvoie la longeur de la chaine de caractère
     return texte.length;
 }
 var remplaceECar = function (texte) {
+  // la fonction 'replace(a,b)' remplace le caractère 'a' par le caractère 'b'
     return texte.replace('e',' ');
 }
 var concatString = function (texte1, texte2) {
+  // concaténation des deux chaines de caractère
     return texte1+texte2;
 }
 var afficherCar5 = function (texte) {
+    // renvoie le caractère à l'indice 4 de la chaine de caractère
     return texte[4];
 }
 var afficher9Car = function (texte) {
+    // la fonction 'substring(a,b)' affiche la chaine de caractere de l'indice 'a' à l'indice 'b'
     return texte.substring(0,9);
 }
 var majusculeString = function (texte) {
+  // 'toUpperCase()' est une fonction qui met la chaine de caractère en majuscule
     return texte.toUpperCase();
 }
 var minusculeString = function (texte) {
+    // 'toLowerCase()' est une fonction qui met la chaine de caractère en minuscule
     return texte.toLowerCase();
 }
 var SupprEspaceString = function (texte) {
-    return 'A completer';
+    // la fonction 'trim()' supprime le premier et le dernier espace de la chaine de caractère
+    return texte.trim();
 }
 var IsString = function (texte) {
-    return 'A completer';
+    // 'typeof' retourne le type de la valeur 'texte'
+    return typeof texte === 'string';
 }
 var AfficherExtensionString = function (texte) {
-    return 'A completer';
+   // 'substring(a,b)' renvoie la chaine de caractère de l'indice 'a' à 'b'; 'lastIndexOf((a),(optionnel))'   renvoi la derniere occurence du caractère 'a'
+   // le 2eme parametre optionnel de la fonction est l'indice du début de la recherche
+    return texte.substring(texte.lastIndexOf('.')+1,texte.length);
 }
 var NombreEspaceString = function (texte) {
+  // 
     return 'A completer';
 }
 var InverseString = function (texte) {
